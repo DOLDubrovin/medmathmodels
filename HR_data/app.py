d36ccd5e56dbab08782a3e20379538c146aa5222
Oleg Dubrovin <DOLDubrovin@gmail.com>
2026-06-15 23:10:05 +0200

    app

--- HR_data/app.py
+++ HR_data/app.py
@@ -1,15 +1,33 @@
-# works with generatedDF.py to create csv files and plots
+import streamlit as st
 import matplotlib.pyplot as plt
 from generatedDF import generate
-df1=generate()
+st.title("Interactive HR Data Generator")
+st.dataframe(df1.head(20))
+st.download_button(...)
+st.pyplot(...)
+n=st.slider("Number",50,5000,200)
+a=st.slider("strength of the influence of the EF on the HR", 0.6, 1.0,0.01)
+df1=generate(n,a)
+st.dataframe(df1) # ?
+st.write("Correlation EF-HR:", df1["EF"].corr(df1["HR"])) # ?
 # print(df1)
+csv=df1.to_csv(index=False).encode("utf-8")
+st.download_button(
+    label="Download csv",
+    data=csv,
+    file_name="df1.csv",
+    mime="text/csv"
+)
+
 plt.figure()
 df1["Age"].hist(bins=10)
 plt.xlabel("Age")
 plt.ylabel("Number of observations")
 plt.title("Distribution of the age")
 plt.savefig("output/plots/age_hist.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
+
 
 plt.figure()
 df1["Sex"].hist()
@@ -17,7 +35,9 @@ plt.xlabel("Sex")
 plt.ylabel("Number of observations")
 plt.title("Distribution of the sex")
 plt.savefig("output/plots/sex_hist.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
+
 
 plt.figure()
 df1["NYHA"].hist()
@@ -25,6 +45,7 @@ plt.xlabel("HYHA")
 plt.ylabel("Number of observations")
 plt.title("Distribution of the Patients by NYHA class")
 plt.savefig("output/plots/NYHA_hist.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
 
 plt.figure()
@@ -44,16 +65,11 @@ plt.xlabel("heart failure Category")
 plt.ylabel("Patients")
 plt.title("Distribution of heart failure category")
 plt.savefig("output/plots/HF_bar.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
 
 
-plt.figure()
-df1["Sex"].hist()
-plt.xlabel("Sex")
-plt.ylabel("Number of observations")
-plt.title("Distribution of the sex")
-plt.savefig("output/plots/sex_hist.png", dpi=300, bbox_inches="tight")
-plt.close()
+
 
 plt.figure()
 df1.boxplot(column="HR", by="NYHA")
@@ -61,6 +77,9 @@ plt.xlabel("NYHA classes")
 plt.ylabel("heart rate")
 plt.title ("heart rate by NYHA classes")
 plt.savefig("output/plots/hr_by_HYHA.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
+plt.close()
+
 
 plt.figure()
 df1.boxplot(column="HR", by="HF")
@@ -68,6 +87,9 @@ plt.xlabel("HF kind")
 plt.ylabel("heart rate")
 plt.title ("heart rate by HF kind")
 plt.savefig("output/plots/hr_by_HF.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
+plt.close()
+
 
 plt.figure()
 plt.scatter(df1["EF"], df1["HR"], alpha=0.6)
@@ -75,7 +97,9 @@ plt.xlabel("Ejection fraction (%)")
 plt.ylabel("Heart rate")
 plt.title("EF versus simulated heart rate")
 plt.savefig("output/plots/ef_vs_hr.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
+
 
 plt.figure()
 plt.scatter(df1["Age"], df1["HR"], alpha=0.6)
@@ -83,4 +107,5 @@ plt.xlabel("Age")
 plt.ylabel("Heart rate")
 plt.title("Age versus simulated heart rate")
 plt.savefig("output/plots/Age_vs_hr.png", dpi=300, bbox_inches="tight")
+st.pyplot(plt)
 plt.close()
--- HR_data/generatedDF.py
+++ HR_data/generatedDF.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import os
 os.makedirs("output/plots",exist_ok=True)
 # %%
-def generate(n=200):   #//number of observations
+def generate(n,a):   #//number of observations n=50 .. 5000, a= 0.6 ..1 (nominal 0.8)
     Age=np.clip(np.random.normal(50,10,n),18,90).astype(int)#//weparate, becouse of use in Functions
     Sex=np.random.choice(["M","F"],n)
     EF=70-Age*0.4+np.random.normal(0,5,n)#//Vermutung: correlation with age, sd 5
@@ -26,7 +26,7 @@ def generate(n=200):   #//number of observations
     categories=["no HF", "HFpEF", "HFmrEF", "HFrEF"],
     ordered=True
     )
-    HR=100-EF*0.8+np.random.normal(0,5,n) # HF empirisch
+    HR=75+(60-EF)*a+np.random.normal(0,5,n) # HF empirisch
     df1 = pd.DataFrame({
 "ObservID":range(1, n+1),#//n+1 besouse the py built to n-1
 "Age":Age,
@@ -38,6 +38,6 @@ def generate(n=200):   #//number of observations
 })
 
     return df1
-df1=generate()
-print(df1)
-df1.to_csv("output/df1.csv", index=False)
+# df1=generate()
+# print(df1)
+
